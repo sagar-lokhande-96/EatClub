bc1ed3ffdf538a0d7253bc00c07c82f21771d687
Sagar Lokhande <sagarlokhande159@gmail.com>
2025-12-29 21:31:30 +0530

    add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,12 +6,13 @@ import userRoutes from "./routes/auth.route.js"
 import foodRoutes from "./routes/food.route.js"
 import cors from 'cors';
 
-const corsOptions = {
-    origin: "http://localhost:5173/",
-    credentials: true
-};
 
 const app = express();
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 app.use(express.json());
 app.use(cookieParser());
 
